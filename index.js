cd7222829ee7bc8a43fa59323ede4d8d8178a892
Kume Yuki <kume@easter.kuee.kyoto-u.ac.jp>
2019-08-20 21:30:31 +0900

    -

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ var app = require('http').createServer(hander);
 var io = require('socket.io').listen(app);
 var fs = require('fs');
 
+app.listen(1337);
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html', function(err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error');
+        }
+        res.writeHead(200);
+        res.write(data);
+        res.end();
+    })
+}
